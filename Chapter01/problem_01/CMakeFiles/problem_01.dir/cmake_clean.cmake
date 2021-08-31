865fc377e6a58e1783cf58670a4ca1dae4abc11d
MDARIFUL HASAN <mhasan22@vols.utk.edu>
2021-08-31 02:33:20 -0400

    compiling codes from chapter01

---
+++ Chapter01/problem_01/CMakeFiles/problem_01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/problem_01.dir/main.cpp.o"
+  "problem_01.pdb"
+  "problem_01"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/problem_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
